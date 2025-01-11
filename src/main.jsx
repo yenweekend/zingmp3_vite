f3eeece987bf0e11148e7395e91d32384efaa1b8
NGUYEN VAN YEN <nguyenvanyen25022004@gmail.com>
2025-01-11 22:05:36 +0700

    fix path issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import GlobalStyle from './Theme/GlobalStyled.jsx';
-import { ToastContainer , Bounce, Slide} from 'react-toastify';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { store, persistor } from './redux/store/index.jsx';
-import { PersistGate } from 'redux-persist/integration/react';
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux';
-const queryClient =  new QueryClient();
-createRoot(document.getElementById('root')).render(
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import GlobalStyle from "./Theme/GlobalStyled.jsx";
+import { ToastContainer, Bounce, Slide } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { store, persistor } from "./redux/store/index.jsx";
+import { PersistGate } from "redux-persist/integration/react";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
       <PersistGate loading={null} persistor={persistor}>
         <Router>
-          <GlobalStyle/>
+          <GlobalStyle />
           <App />
           <ToastContainer
-          position="top-center"
-          autoClose={2000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover={false}
-          theme="colored"
-          transition={Slide}
+            position="top-center"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover={false}
+            theme="colored"
+            transition={Slide}
           />
         </Router>
       </PersistGate>
     </QueryClientProvider>
   </Provider>
-)
+);
